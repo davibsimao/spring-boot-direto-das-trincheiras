bff3a58bb47d73d5a17d290d5fb66f6e2ab0fc0f
Davi Simão <davbsimao@gmail.com>
2026-08-21 15:15:43 -0300

    feat(video11): requisicoes usando request param

--- anime-service/src/main/java/academy/devdojo/controllers/AnimesController.java
+++ anime-service/src/main/java/academy/devdojo/controllers/AnimesController.java
@@ -1,5 +1,6 @@
 package academy.devdojo.controllers;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,11 +9,12 @@ import java.util.List;
 
 @RestController()
 @RequestMapping("v1/animes")
+@Slf4j
 public class AnimesController {
 
     @GetMapping()
     public List<String> listAll() {
-
+        log.info(Thread.currentThread().getName());
         return List.of("naruto", "nanatsu", "demon slayer", "attack on titan");
     }
 }
