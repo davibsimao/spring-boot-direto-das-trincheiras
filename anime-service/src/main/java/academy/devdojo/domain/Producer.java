ddb2b2359cb3fa057d345a9e5710faf86c9b10b3
Davi Simão <davbsimao@gmail.com>
2026-08-24 14:39:39 -0300

    refactor(video18): Desacoplando dominio de requisicoes e respostas

--- anime-service/src/main/java/academy/devdojo/domain/Producer.java
+++ anime-service/src/main/java/academy/devdojo/domain/Producer.java
@@ -2,26 +2,41 @@ package academy.devdojo.domain;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
 @Getter
 @Setter
-@AllArgsConstructor
+@Builder
 public class Producer {
     private Long id;
     @JsonProperty("name")
     private String name;
+    private LocalDateTime createdAt;
     @Getter
     private static List<Producer> producers = new ArrayList<>();
 
     static {
-        Producer mappa = new Producer(1L, "Mappa");
-        Producer kyotoAnimation = new Producer(2L, "Kyoto Animation");
-        Producer madHouse = new Producer(3L, "MadHouse");
+        Producer mappa = Producer.builder().id(1L)
+                .name("Mappa")
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        Producer kyotoAnimation = Producer.builder().id(2L)
+                .name("Kyoto Animation")
+                .createdAt(LocalDateTime.now())
+                .build();
+
+        Producer madHouse = Producer.builder().id(3L)
+                .name("MadHouse")
+                .createdAt(LocalDateTime.now())
+                .build();
+        
         producers.addAll(List.of(mappa, kyotoAnimation, madHouse));
     }
 }
