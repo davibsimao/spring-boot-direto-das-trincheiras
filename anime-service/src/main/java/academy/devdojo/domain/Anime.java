46315104e0047f3bcd20d0d7f5c53afff77b9c74
Davi Simão <davbsimao@gmail.com>
2026-08-24 13:52:36 -0300

    refactor(video17): adicionando  Response Entity no save

--- anime-service/src/main/java/academy/devdojo/domain/Anime.java
+++ anime-service/src/main/java/academy/devdojo/domain/Anime.java
@@ -13,6 +13,7 @@ import java.util.List;
 public class Anime {
     private Long id;
     private String name;
+    @Getter
     private static List<Anime> animes = new ArrayList<>();
 
     static {
@@ -23,7 +24,4 @@ public class Anime {
         animes.addAll(List.of(naruto, demonSlayer, dragonBall, attackOnTitan));
     }
 
-    public static List<Anime> getAnimes() {
-        return animes;
-    }
 }
