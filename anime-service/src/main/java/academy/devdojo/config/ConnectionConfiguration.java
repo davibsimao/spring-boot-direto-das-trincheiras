e8236936fa16b246927e27637875d5e1324c7efa
Davi Simão <davbsimao@gmail.com>
2026-09-14 16:07:58 -0300

    feat(video49): configurando perfis mysql e mongo no application.yml

--- anime-service/src/main/java/academy/devdojo/config/ConnectionConfiguration.java
+++ anime-service/src/main/java/academy/devdojo/config/ConnectionConfiguration.java
@@ -1,19 +1,26 @@
 package academy.devdojo.config;
 
 import external.dependency.Connection;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ConnectionConfiguration {
+    @Value("${database.url}")
+    private String url;
+    @Value("${database.username}")
+    private String username;
+    @Value("${database.password}")
+    private String password;
 
     @Bean
     public Connection connectionMySql() {
-        return new Connection("localhost", "devdojo", "goku");
+        return new Connection(url, username, password);
     }
+
     @Bean(name = "connectionMongoDB")
     public Connection connectionMongo() {
         return new Connection("localhost", "devdojoMongo", "goku");
     }
-
 }
